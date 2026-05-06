cb041742ae402792f43bbde102ab17b07c880e8b
Sarthak1976 <sarthakw196@gmail.com>
2026-05-06 11:27:47 +0530

    Refactor main.ts to implement task management

--- W-12 Angular todo/main.ts
+++ W-12 Angular todo/main.ts
@@ -1,6 +1,45 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,FormsModule,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  newTask: string = '';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  tasks: string[] = [];
+
+  successMsg: string = '';
+  errorMsg: string = '';
+
+  addTask() {
+    if (this.newTask.trim() !== ""){
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+      this.successMsg = 'Task saved successfully !'
+      this.errorMsg = '';
+      setTimeout(() => {
+        this.successMsg = '';
+      }, 2000);
+    }else{
+      this.errorMsg = 'Please enter a task first!';
+      this.successMsg = '';
+
+      setTimeout(() => {
+        this.errorMsg = '';
+      }, 2000);
+    }
+  }
+
+  editTask(i:number){
+    this.newTask = this.tasks[i];
+    this.deleteTask(i);
+  }
+  deleteTask(i:number){
+    this.tasks.splice(i,1);
+  }
+}
